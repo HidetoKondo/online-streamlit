8b2307efef1fdb61c6de00edf19f24ac6ab522f8
HidetoKondo <eagle@hmi117.com>
2023-01-09 22:10:22 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
 import streamlit as st
-import numpy as np
-import pandas as pd
 import time as te
-from PIL import  Image
-#from streamlit_metrics import metric, metric_row
-
 
 st.title('Streamlit 超入門')
-st.write('Interactive Widgets')
 st.write('プログレスバーの表示')
 'Start!!'
 
@@ -17,7 +11,7 @@ bar = st.progress(0)
 for i in range(100):
 #    latest_iteration.text(f'Iteration {i+1}')
 #    bar.progress(i + 1)
-    te.sleep(0.1)
+    te.sleep(0.02)
     bar.progress(i + 1)
 
 'Done!!'
@@ -34,15 +28,3 @@ expander2.write('問い合わせ2の回答')
 expander3 = st.expander('問い合わせ3')
 expander3.write('問い合わせ3の回答')
 
-
-"""
-# 章
-## 節
-### 項
-
-```python
-import streamlit as st
-import numpy as np
-import pandas as pd
-```
-"""
